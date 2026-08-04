627c8075a4e76af041057b07b0528baa80b0bdd4
shivansh pareek <136789037+Shivanash24@users.noreply.github.com>
2026-08-04 18:03:17 +0530

    fix: use local SVG files and fix image sizing for trust logos

--- src/components/TrustLogos.tsx
+++ src/components/TrustLogos.tsx
@@ -3,12 +3,12 @@
 import { motion } from "framer-motion";
 
 const logos = [
-  { name: "Meta", src: "https://upload.wikimedia.org/wikipedia/commons/7/7b/Meta_Platforms_Inc._logo.svg", width: 140 },
-  { name: "Google", src: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg", width: 110 },
-  { name: "HubSpot", src: "https://upload.wikimedia.org/wikipedia/commons/e/ec/HubSpot_Logo.svg", width: 120 },
-  { name: "Salesforce", src: "https://upload.wikimedia.org/wikipedia/commons/f/f9/Salesforce.com_logo.svg", width: 100 },
-  { name: "WordPress", src: "https://upload.wikimedia.org/wikipedia/commons/0/09/Wordpress_Logo.svg", width: 160 },
-  { name: "Shopify", src: "https://upload.wikimedia.org/wikipedia/commons/0/0e/Shopify_logo_2018.svg", width: 120 },
+  { name: "Meta", src: "/logos/meta.svg", width: 140 },
+  { name: "Google", src: "/logos/google.svg", width: 110 },
+  { name: "HubSpot", src: "/logos/hubspot.svg", width: 120 },
+  { name: "Salesforce", src: "/logos/salesforce.svg", width: 100 },
+  { name: "WordPress", src: "/logos/wordpress.svg", width: 160 },
+  { name: "Shopify", src: "/logos/shopify.svg", width: 120 },
 ];
 
 export default function TrustLogos() {
@@ -43,8 +43,8 @@ export default function TrustLogos() {
                 <img 
                   src={logo.src} 
                   alt={`${logo.name} logo`} 
-                  style={{ width: logo.width, objectFit: 'contain' }}
-                  className="max-h-full"
+                  style={{ width: logo.width }}
+                  className="h-auto max-h-12 object-contain block"
                 />
               </div>
             ))}
